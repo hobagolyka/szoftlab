c9ec45d535ba664092e587237de33d5c7ef2a1bc
hobagolyka <toth.jdt@gmail.com>
2016-04-23 20:32:54 +0200

    replicator osztály

---
+++ xcy/src/Replicator.java
@@ -0,0 +1,24 @@
+/**
+ * Created by Judit on 2016. 04. 23..
+ */
+public class Replicator implements Movable {
+    private Floor position;
+    private Direction direction;
+
+    @Override
+    public void step() {
+
+    }
+
+    @Override
+    public void setPosition(Floor position) {
+        this.position = position;
+    }
+
+    @Override
+    public Floor getPosition() {
+        return position;
+    }
+
+    public void die()[{}
+}
